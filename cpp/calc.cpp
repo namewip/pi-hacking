e353278b52ca44d49adfbfbafebc94191e69aa60
z2s8 <szanto.zsombor@gmail.com>
2015-04-03 22:27:44 +0200

    Add hyperfast C++ calculator in here!
    via MPFRC++
    
    Squashed commit of the following:
    
    commit c6511840b8e9929df9092d129c342c93cff5f1bd
    Author: z2s8 <szanto.zsombor@gmail.com>
    Date:   Fri Apr 3 21:08:40 2015 +0200
    
        Fix typo in travis afterbuild script
    
    commit 5c2c94977e1b5061f24f07f134686b554d85eed6
    Author: z2s8 <szanto.zsombor@gmail.com>
    Date:   Fri Apr 3 20:54:08 2015 +0200
    
        Preparing merge, finishig up branch.
    
    commit 56465c1690c94edc5c4651c1693cc7c3fe2ce393
    Author: z2s8 <szanto.zsombor@gmail.com>
    Date:   Fri Apr 3 20:27:47 2015 +0200
    
        Minor travis & style fixes.
    
    commit 9c908dc2b6b627a7a65f1c605a0cf170ef125e77
    Author: z2s8 <szanto.zsombor@gmail.com>
    Date:   Fri Apr 3 20:08:10 2015 +0200
    
        Add slack notifis to travis
    
        Another attempt to fix g++ ;)
    
    commit 286ca7e49e434ac28b4acbb626b060a9cbe8a7fb
    Author: z2s8 <szanto.zsombor@gmail.com>
    Date:   Fri Apr 3 15:34:26 2015 +0200
    
        Falling back to c++11 as 14 isn't yet supported in ubuntu 12.04
    
    commit 3b52a1582be2ef57dc0c8e7fa150acdbf77048f1
    Author: z2s8 <szanto.zsombor@gmail.com>
    Date:   Fri Apr 3 15:29:18 2015 +0200
    
        This should really fix apt
    
    commit 5434692c88b87ac3f82d3945a6beaf885ed8ce46
    Author: z2s8 <szanto.zsombor@gmail.com>
    Date:   Fri Apr 3 15:20:11 2015 +0200
    
        Fix travis upgrading rsyslog causing build to hang
    
    commit 4a26ce4e00c8d76c0307741a8738f1f6f53a50e1
    Author: z2s8 <szanto.zsombor@gmail.com>
    Date:   Fri Apr 3 15:07:39 2015 +0200
    
        Prepare travis
    
    commit 1a599b9c50286cf84f4832b830824e713f94f4dd
    Author: z2s8 <szanto.zsombor@gmail.com>
    Date:   Fri Apr 3 14:34:21 2015 +0200
    
        Add hyperfast C++ code in  here!
    
        using MPFRC++

---
+++ cpp/calc.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "mpreal.h"
+
+mpfr::mpreal fact(int n) {
+	return (n == 0 || n == 1) ? 1 : fact(n - 1) * n;
+}
+
+mpfr::mpreal calcit(int maxiter) {
+
+	mpfr::mpreal s = mpfr::mpreal(0);
+
+	for (int n = 0; n < maxiter; ++n) {
+		s += mpfr::pow(-1, n) * fact(6 * n) * (13591409 + n * mpfr::mpreal(545140134)) /
+             mpfr::pow(fact(n), 3) / fact(3 * n) /
+             mpfr::pow(100100025 * 8 * mpfr::mpreal(327843840), n);
+	}
+
+	return 53360 * mpfr::pow(640320, mpfr::mpreal("0.5")) / s; 
+
+}
+
+int main(int argc, char *argv[]) {
+
+	const int prec = 7;
+
+	mpfr::mpreal::set_default_prec(50 * 50 * prec);
+	std::cout.precision(50 * 75 * prec);
+
+	std::cout << calcit(50 * prec) << std::endl << mpfr::mpreal::get_default_prec();
+
+    return 0;
+}
