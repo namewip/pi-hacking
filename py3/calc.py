46e3f4ecea1b52e7a86b3a6700cb836b4e60effb
z2s8 <szanto.zsombor@gmail.com>
2015-04-09 01:03:24 +0200

    autopep8 beatifulled

--- py3/calc.py
+++ py3/calc.py
@@ -4,12 +4,14 @@ import math
 prec = 7
 decimal.getcontext().prec = 2500 * prec
 
+
 def calcit(maxiter):
-	s = decimal.Decimal(0)
+    s = decimal.Decimal(0)
 
-	for n in range(maxiter):
-		s += (-1) ** n * math.factorial(6 * n) * (13591409 + n * decimal.Decimal(545140134)) / (math.factorial(n) ** 3) / math.factorial(3 * n) / (100100025 * 8 * decimal.Decimal(327843840)) ** n
+    for n in range(maxiter):
+        s += (-1) ** n * math.factorial(6 * n) * (13591409 + n * decimal.Decimal(545140134)) / \
+            (math.factorial(n) ** 3) / math.factorial(3 * n) / (100100025 * 8 * decimal.Decimal(327843840)) ** n
 
-	return 53360 * decimal.Decimal(640320).sqrt() / s
+    return 53360 * decimal.Decimal(640320).sqrt() / s
 
 print(calcit(50 * prec))
