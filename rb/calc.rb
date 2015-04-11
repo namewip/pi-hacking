93b29a87d02c2ec944b4c1c8b913f950db5e608c
z2s8 <szanto.zsombor@gmail.com>
2015-04-11 23:16:56 +0200

    Add ruby

---
+++ rb/calc.rb
@@ -0,0 +1,28 @@
+require 'bigdecimal'
+
+class Integer
+	def fact
+		f = 1
+		for i in 1..self
+			f *= i
+		end
+		f
+  	end
+end
+
+prec = 15
+BigDecimal::mode(BigDecimal::ROUND_MODE, BigDecimal::ROUND_HALF_UP)
+@decprec = 2500 * prec
+
+def calcit(maxiter)
+	s = BigDecimal.new('0')
+	
+	maxiter.times do |n|
+		s += (-1) ** n * (6 * n).fact * (13591409 + n * BigDecimal.new(545140134, @decprec)) /
+			(n.fact ** 3) / (3 * n).fact / (100100025 * 8 * BigDecimal.new(327843840, @decprec)) ** n
+	end
+
+	return 53360 * BigDecimal.new(640320, @decprec).sqrt(2) / s
+end
+
+puts(calcit(50 * prec).to_s("#{3750 * prec}F"))
