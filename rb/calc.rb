634d4f48d499fb7fab9d68d8c6736ffee239406e
z2s8 <szanto.zsombor@gmail.com>
2015-12-08 00:14:17 +0100

    Ruby precision tweaks, maybe

--- rb/calc.rb
+++ rb/calc.rb
@@ -10,19 +10,19 @@ class Integer
   	end
 end
 
-prec = 15
+prec = 3
 BigDecimal::mode(BigDecimal::ROUND_MODE, BigDecimal::ROUND_HALF_UP)
 @decprec = 2500 * prec
 
 def calcit(maxiter)
-	s = BigDecimal.new('0')
+	s = BigDecimal.new('0', @decprec)
 	
 	maxiter.times do |n|
 		s += (-1) ** n * (6 * n).fact * (13591409 + n * BigDecimal.new(545140134, @decprec)) /
-			(n.fact ** 3) / (3 * n).fact / (100100025 * 8 * BigDecimal.new(327843840, @decprec)) ** n
+			n.fact ** 3 / (3 * n).fact / (100100025 * 8 * BigDecimal.new(327843840, @decprec)) ** n
 	end
 
-	return 53360 * BigDecimal.new(640320, @decprec).sqrt(2) / s
+	return 53360 * BigDecimal.new(640320, @decprec) ** BigDecimal.new('0.5', @decprec) / s
 end
 
 puts(calcit(50 * prec).to_s("#{3750 * prec}F"))
